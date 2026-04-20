a1f1efea825143c9d599afe53d6e031ce47ffa59
nishant <nishant.nish20@gmail.com>
2026-04-21 00:50:22 +0530

    change

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ console.log(5-5);
 console.log(5*5);
 console.log(5/5);
 console.log('hello wrold')
+
+
+let age = 30;
+const name ="nishu";
+var city = "uttar pradesh";
+var city = "delhi";
+console.log(name);
+console.log(age);
+console.log(city);
